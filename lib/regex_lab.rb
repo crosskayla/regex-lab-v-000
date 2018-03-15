9ce4f7b208c3c4413eca7aa72b4bf731c20d219f
Kayla Cross <kac288@georgetown.edu>
2018-03-15 02:31:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,6 +12,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  binding.pry
   return true if text.match(/[A-Z][^\.!\?]*[\.!\?]/)
   false
 end
