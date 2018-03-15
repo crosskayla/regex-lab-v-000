0978154dc285b6f7558341a13a8ccc45f88046db
Kayla Cross <kac288@georgetown.edu>
2018-03-15 02:24:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,11 +12,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/[A-Z][^\.!\?]*[\.!\?]/)
-    return true
-  else
-    return false
-  end
+  return true if text.match(/[A-Z][^\.!\?]*[\.!\?]/)
+  false
 end
 
 def valid_phone_number?(phone)
